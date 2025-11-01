c33e305840094f05add502fb5c600ef6d7d63821
Bakhshish <bakhshishsingh2001@gmail.com>
2025-11-01 23:21:35 +0530

    update

--- server/server.js
+++ server/server.js
@@ -160,7 +160,7 @@ mongoose
   .catch((err) => console.log("MongoDB Connection Failed:", err));
 
 // Start the server
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
